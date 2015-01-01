cb482d917fda58b819c6350a9094a0b00748d93f
BhavyaMhr <buddymehra@hotmail.com>
2015-01-01 21:50:04 +0530

    datbs

---
+++ bboooks.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 01, 2015 at 05:15 PM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `bboooks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirmation`
+--
+
+CREATE TABLE IF NOT EXISTS `confirmation` (
+  `OTP` varchar(32) NOT NULL,
+  `Email` varchar(40) DEFAULT NULL,
+  `Password` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`OTP`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `Username` varchar(30) DEFAULT NULL,
+  `Password` varchar(32) DEFAULT NULL,
+  KEY `fk_Username` (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`Username`, `Password`) VALUES
+('harmandeepsinghkalsi9', '99d0556a10eee503a341bac59a272903'),
+('bhavyamehra', 'e90c6647830e603b4e761311d05238db'),
+('sahibpreetsingh94', 'e90c6647830e603b4e761311d05238db');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master`
+--
+
+CREATE TABLE IF NOT EXISTS `master` (
+  `Username` varchar(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `ContactNo` varchar(12) DEFAULT NULL,
+  `Qualification` varchar(50) DEFAULT NULL,
+  `Profession` varchar(30) DEFAULT NULL,
+  `Link_Photo` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`Username`),
+  UNIQUE KEY `u_EMail` (`Email`),
+  UNIQUE KEY `u_ContactNo` (`ContactNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master`
+--
+
+INSERT INTO `master` (`Username`, `Name`, `Email`, `ContactNo`, `Qualification`, `Profession`, `Link_Photo`) VALUES
+('bhavyamehra', 'Bhavya Mehra', 'buddymehra@yahoo.com', '8789544650', NULL, NULL, NULL),
+('harmandeepsinghkalsi9', 'Harmandeep Singh Kalsi', 'harmandeepsinghkalsi9@gmail.com', '9888518432', 'BTech', 'Student', 1),
+('sahibpreetsingh94', 'Sahibpreet Singh', 'jsbhalla52@gmail.com', '', '', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `ID` int(10) NOT NULL,
+  `Title` varchar(50) DEFAULT NULL,
+  `Subject` varchar(30) DEFAULT NULL,
+  `Author` varchar(50) DEFAULT NULL,
+  `Edition` int(3) DEFAULT NULL,
+  `Original_Price` int(4) DEFAULT NULL,
+  `Selling_Price` int(4) DEFAULT NULL,
+  `Photo` tinyint(1) DEFAULT NULL,
+  `Username` varchar(30) DEFAULT NULL,
+  `dateofpost` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`ID`, `Title`, `Subject`, `Author`, `Edition`, `Original_Price`, `Selling_Price`, `Photo`, `Username`, `dateofpost`) VALUES
+(0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2014-12-31 00:14:39'),
+(1, 'test1', 'subject1', 'Admin', 1, 456, 985, 1, 'sahibpreetsingh94', '2014-12-31 00:31:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp`
+--
+
+CREATE TABLE IF NOT EXISTS `temp` (
+  `Username` varchar(30) DEFAULT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(40) NOT NULL,
+  `ContactNo` varchar(12) DEFAULT NULL,
+  `OTP` varchar(32) DEFAULT NULL,
+  KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `login`
+--
+ALTER TABLE `login`
+  ADD CONSTRAINT `fk_Username` FOREIGN KEY (`Username`) REFERENCES `master` (`Username`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `temp`
+--
+ALTER TABLE `temp`
+  ADD CONSTRAINT `fk_Email` FOREIGN KEY (`Email`) REFERENCES `confirmation` (`Email`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
